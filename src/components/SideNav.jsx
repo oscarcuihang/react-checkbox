2af939e85b9ab9b46038a837ec227a7b1bcf3bfe
Hang Cui <oscarcuihang@hotmail.com>
2018-08-25 16:56:17 -0500

    Create Wrapper

---
+++ src/components/SideNav.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SideNav extends Component {
+  render() {
+    return (
+      <div className="SideNav">
+        <h1>qdwasdasdasd</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
