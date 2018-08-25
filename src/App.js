cea47c091a78cfbc4f36abea60beb37a3bd464cf
Hang Cui <oscarcuihang@hotmail.com>
2018-08-25 18:23:33 -0500

    Creat wrapper (#1)
    * Create Wrapper

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SideNav from './components/SideNav'
+import Grid from '@material-ui/core/Grid';
+import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <div>
+          <Grid container spacing={24}>
+            <Grid item xs={4} className='App-navbar-color'><SideNav /></Grid>
+          </Grid>
       </div>
     );
   }
---
+++ src/components/SideNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SideNav extends Component {
+  render() {
+    return (
+      <div className="SideNav">
+        <h1>招聘职位</h1>
+      </div>
+    );
+  }
+}
+
+export default SideNav;
