2e7b039bf8292e4d17a363c5b090cbc8128d98af
Hang Cui <oscarcuihang@hotmail.com>
2018-08-25 20:29:34 -0500

    Create Hiring Position Component

--- src/components/HiringPosition.js
+++ src/components/HiringPosition.js
@@ -3,6 +3,20 @@ import Button from '@material-ui/core/Button';
 import './HiringPosition.css'
 
 class HiringPosition extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hiringPostions: null,
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/oscarcuihang/react-checkbox/master/data/hiring_position.json";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => this.setState({ hiringPostions: data }));
+  }
+
   render() {
     return (
       <div className="HiringPosition">
